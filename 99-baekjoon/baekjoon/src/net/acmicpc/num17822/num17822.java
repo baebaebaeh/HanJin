2a99d97e3685bfa30977e226a0d1a028c0189d74
baehanjin <bae9954@naver.com>
2024-08-21 00:17:27 +0900

    0821-1

---
+++ 99-baekjoon/baekjoon/src/net/acmicpc/num17822/num17822.java
@@ -0,0 +1,16 @@
+package net.acmicpc.num17822;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.StreamTokenizer;
+import java.util.StringTokenizer;
+
+public class num17822 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+	}
+}
