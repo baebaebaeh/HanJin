108773b1869364c810e22155a5d11f0e4fb3b1ce
JPHong <hsc3757@gmail.com>
2024-08-21 17:48:26 +0900

    0821-2

--- 99-baekjoon/baekjoon/src/net/acmicpc/num17822/num17822.java
+++ 99-baekjoon/baekjoon/src/net/acmicpc/num17822/num17822.java
@@ -7,10 +7,44 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.StreamTokenizer;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class num17822 {
+	static int[][] map;
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.setIn(new FileInputStream("data/input17822.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		st = new StringTokenizer(br.readLine());
+		int x = Integer.parseInt(st.nextToken()); // 배수
+		int d = Integer.parseInt(st.nextToken()); // 방향
+		int k = Integer.parseInt(st.nextToken()); // 몇번
 		
+		if (d == 1) {
+			k = N - k;
+		}
+		int temp = 0;
+		for (int i = x - 1; i < N; i += x) {
+			int[] replace = new int[M];
+			for (int j = 0; j < M; j++) {
+				temp = j + k;
+				temp %= M;
+				replace[j] = map[i][temp];
+			}
+			map[i] = replace;
+		}
+		System.out.println(Arrays.deepToString(map));
 	}
 }
